f470edcad4f21dc198df2cae264146d659c360b1
steryo <devhochi@gmail.com>
2023-12-04 14:57:40 +0300

    more tweaks
    tweaks on reflection and lights,
    also finished debug & disabled all dynamic lights' shadows

--- Assets/CaseAssets/LightFlicker.cs
+++ Assets/CaseAssets/LightFlicker.cs
@@ -10,7 +10,6 @@ public class LightFlicker : MonoBehaviour
     [SerializeField] private bool moveEnabled;
     [SerializeField] private float moveAmount;
     [SerializeField] private float moveDuration;
-
     void Start()
     {
         lightSelf = gameObject.GetComponent<Light>();
@@ -26,16 +25,14 @@ public class LightFlicker : MonoBehaviour
         parallelSequence.SetLoops(-1, LoopType.Yoyo);
         parallelSequence.Play();
     }
-
     private Tween FlickLight()
     {
         return DOTween.To(() => lightSelf.intensity, startIntensity => lightSelf.intensity = startIntensity, endIntensity, lightDuration)
             .SetEase(Ease.InOutSine);
     }
-
     private Tween MoveLoop()
     {
-        return transform.DOMoveZ(transform.position.z + moveAmount, moveDuration)
+        return transform.DOMoveY(transform.position.y + moveAmount, moveDuration)
             .SetEase(Ease.InOutSine);
     }
 }
