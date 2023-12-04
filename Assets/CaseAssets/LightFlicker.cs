6ced894779a76c2450c34cb3c63d48c495307526
steryo <devhochi@gmail.com>
2023-12-04 05:04:32 +0300

    tweaks
    * fireplace flame renewed
    * fixes on reflective surfaces
    * moving dynamic lights
    * compression on textures

--- Assets/CaseAssets/LightFlicker.cs
+++ Assets/CaseAssets/LightFlicker.cs
@@ -1,23 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
 public class LightFlicker : MonoBehaviour
 {
-private Light lightSelf;
-[SerializeField] private float startIntensity;
-[SerializeField] private float endIntensity;
-[SerializeField] private float duration;
+    private Light lightSelf;
+    [SerializeField] private float startIntensity;
+    [SerializeField] private float endIntensity;
+    [SerializeField] private float lightDuration;
+    [SerializeField] private bool moveEnabled;
+    [SerializeField] private float moveAmount;
+    [SerializeField] private float moveDuration;
+
     void Start()
     {
         lightSelf = gameObject.GetComponent<Light>();
-        FlickLight();
+
+        Sequence parallelSequence = DOTween.Sequence();
+        Tween flickerTween = FlickLight();
+        parallelSequence.Append(flickerTween);
+        if (moveEnabled)
+        {
+            Tween moveTween = MoveLoop();
+            parallelSequence.Insert(0, moveTween);
+        }
+        parallelSequence.SetLoops(-1, LoopType.Yoyo);
+        parallelSequence.Play();
     }
-    private void FlickLight()
+
+    private Tween FlickLight()
     {
-         DOTween.To(() => lightSelf.intensity, startIntensity => lightSelf.intensity = startIntensity, endIntensity, duration)
-            .SetEase(Ease.InOutSine)
-            .SetLoops(-1, LoopType.Yoyo);
+        return DOTween.To(() => lightSelf.intensity, startIntensity => lightSelf.intensity = startIntensity, endIntensity, lightDuration)
+            .SetEase(Ease.InOutSine);
+    }
+
+    private Tween MoveLoop()
+    {
+        return transform.DOMoveZ(transform.position.z + moveAmount, moveDuration)
+            .SetEase(Ease.InOutSine);
     }
 }
